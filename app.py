1feef5a1b3aa84b149ba6625a4661df6193bfb43
Nathan A Duvenick <51921396+nadbn4@users.noreply.github.com>
2020-10-23 14:20:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,57 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objs as go
 
+########### read in data
+win_loss_df = pd.read_csv("win_loss_df.csv").sort_values(['wins', 'points_for'], ascending = False)
+
+myheading = 'Raytonia Beach Fantasy Football League'
+tabtitle='Raytown!'
+
+########### Set up the chart
+win_bars = go.Bar(x=win_loss_df['owner_team_name'], y=win_loss_df['wins'],
+                  base=0,
+                  marker_color='green',
+                  name='Wins',
+                  hoverinfo='y'
+)
+
+loss_bars = go.Bar(x=win_loss_df['owner_team_name'], y=win_loss_df['losses'],
+                   base= -1 * win_loss_df['losses'].astype('int'),
+                   text = 1 * win_loss_df['losses'].astype('int'),
+                   marker_color='red',
+                   name='Losses',
+                   hoverinfo='text'
+)
+
+win_loss_data = [win_bars, loss_bars]
+
+#fig.update_yaxes(tickvals=[5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5], ticktext = [5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5])
+
+win_loss_layout = go.Layout(barmode = 'overlay',
+                            hovermode = 'x',
+                            title = 'Wins/Losses'
+)
+
+win_loss_fig = go.Figure(data=win_loss_data, layout=win_loss_layout)
+
+########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
+########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
+    html.H1(myheading),
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='win_loss_fig',
+        figure=win_loss_fig
     )
-])
+    ]
+)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
